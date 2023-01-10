36ac56201b96819778d68ff3329342a94ab7dc6a
Avishisht <avishishtgupta@gmail.com>
2023-01-10 21:04:41 +0530

    try 4

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-const request = require("request");
+// const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
-app.use("/", (req, res) => {
-  res.json({message: "Hellosss"});
-});
-
 
 app.get("/", function(req,res) {
     res.sendFile(__dirname + "/index.html")
