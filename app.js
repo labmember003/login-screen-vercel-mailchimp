eebb67dbb04f1278e59179fe99eca15d0a7af5f5
Avishisht <avishishtgupta@gmail.com>
2023-01-10 17:56:24 +0530

    try 3

--- app.js
+++ app.js
@@ -63,11 +63,11 @@ app.post("/failure", function(req, res) {
 
 
 // process.env.PORT is a dynamic port
-const port = process.env.PORT || 90001;
+const port = process.env.PORT || 9001;
 // app.listen(port, function(){
 //   console.log("Server running")
 // });
-app.listen(port, () => console.log(`Listening`)))
+app.listen(port, () => console.log(`Listening`))
 //583c6ef5d1866066044e07baa890e3e9-us18
 //Audience ID
 //b764cd5a29
