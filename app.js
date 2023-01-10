f8cfc502a1acc42e2539ba4c6616de562f452110
Avishisht <avishishtgupta@gmail.com>
2023-01-10 19:06:21 +0530

    added use block

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use("/", (req, res) => {
+  res.json({message: "Hellosss"});
+});
+
+
 app.get("/", function(req,res) {
     res.sendFile(__dirname + "/index.html")
 });
